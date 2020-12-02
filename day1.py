f92393d60ea3e6b67dbc42a3daab1b9c84906559
Konrad Rzepkowski <konradr@ims.int>
2020-12-02 18:09:33 +0100

    Part 2

--- day1.py
+++ day1.py
@@ -4,7 +4,9 @@ with open("1.txt.txt", "r") as file_:
 numbers = numbers.split("\n")
 for number in numbers:
     for i in range(len(numbers)):
-        wynik= int(number) + int(numbers[i])
-        if wynik == 2020:
-            print(int(number)*int(numbers[i]))
+        for x in range(len(numbers)):
+            wynik= int(number) + int(numbers[i]) + int(numbers[x])
+            if wynik == 2020:
+                answer = int(number)*int(numbers[i])*int(numbers[x])
+print(answer)
 
