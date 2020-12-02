d85285e7490e869c5abd7d8fa6853668ca825a8b
Konrad Rzepkowski <konradr@ims.int>
2020-12-02 18:02:58 +0100

    Part 2

--- day2.py
+++ day2.py
@@ -10,7 +10,12 @@ for passwd in passwords:
     letter = password[0][-1]
     minimal = int(password[0].split("-")[0])
     maximal = int(password[0].split("-")[1].split(" ")[0])
-    if string.count(letter) >= minimal and string.count(letter) <= maximal:
+    #if string.count(letter) >= minimal and string.count(letter) <= maximal:
+    #    count +=1
+    if string[minimal] == letter or string[maximal] == letter:
         count +=1
+        if string[minimal] == letter and string[maximal] == letter:
+            count -=1
+
 print(count)
 
